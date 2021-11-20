52c78a2ad794798d8456a453016ccaf43da29f70
Braden MacBeth <bradenmacbeth@outlook.com>
2021-11-20 14:01:40 -0500

    added packages and some files

---
+++ Transfiguration/Database.fs
@@ -0,0 +1,14 @@
+﻿module Database
+open Config
+open System.Data
+open System.Data.SqlClient
+open Dapper.FSharp
+open Dapper.FSharp.MSSQL
+
+// Not sure if I am going to use this.
+let UpdateQuery query = 
+    let conn = new SqlConnection (Config.dbconn)
+    conn.OpenAsync() |> ignore
+    let result = query |> conn.UpdateAsync
+    conn.CloseAsync() |> ignore
+    result
