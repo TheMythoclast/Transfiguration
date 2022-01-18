57939246e017d335371a4690cd3e890f1493ed92
Braden MacBeth <bradenmacbeth@outlook.com>
2022-01-18 15:46:35 -0500

    cleaning up

--- Transfiguration/Applicant/Applicant.fs
+++ Transfiguration/Applicant/Applicant.fs
@@ -1,4 +1,31 @@
 ﻿module Applicant
 open System
+open Database
 open Dapper.FSharp
 open Dapper.FSharp.MSSQL
+
+let private ApplicantTable = table<Applicant>
+
+let CreateApplicant applicant = 
+    insert {
+    into ApplicantTable
+    value applicant } |> Database.InsertQuery
+
+
+let UpdateApplicant Applicant = 
+    update {
+    for a in ApplicantTable do 
+        set Applicant
+        where(a.UserID = Applicant.UserID) } |> UpdateQuery
+
+
+let DeleteApplicant UserID = delete { 
+    for a in ApplicantTable do 
+        where(a.UserID = UserID) } |> DeleteQuery
+
+let GetApplicant UserID = 
+    select { 
+    for a in ApplicantTable do
+    where(a.UserID = a.UserID)
+    } |> SelectQuery
+
--- Transfiguration/Program/ResProgram.fs
+++ Transfiguration/Program/ResProgram.fs
@@ -5,3 +5,10 @@ open Dapper.FSharp.MSSQL
 
 let ProgramTable = table<Program>
 
+let GetPrograms p = select { 
+    for p in ProgramTable do 
+        selectAll } |> SelectQuery
+
+let GetProgramsBySpecialty SpecialtyID = select { 
+    for p in ProgramTable do    
+    where(p.Specialty = SpecialtyID) } |> SelectQuery
